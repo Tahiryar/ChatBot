4548dac48a7d13d3fd50e5ed8b43ffaa4301f039
Tahiryar <tahiryar.251k@gmail.com>
2025-06-23 19:32:54 +0500

    use while loop & input from user and then generate data

--- main.py
+++ main.py
@@ -6,3 +6,18 @@ genai.configure(api_key="API_Key")
 #  Load model
 model = genai.GenerativeModel('gemini-1.5-flash')
 
+
+print(" Gemini Chatbot Ready! Type 'bye' to exit.\n")
+
+#  Loop for chat
+while True:
+    user_input = input("You: ")
+    if user_input.lower() == "bye":
+        print("Bot: Bye! Have a great day ")
+        break
+
+    try:
+        response = model.generate_content(user_input)
+        print("Bot:", response.text.strip())
+    except Exception as e:
+        print(" Error:", e)
